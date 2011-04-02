d65229134a19633f401847a33c89d8a95e754528
carvil <carlosvilhena@gmail.com>
2011-04-02 16:48:25 +0100

    - Added rails skeleton app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babystats::Application.config.secret_token = 'c015bcc583b835d38cd1c0fc530c73548ac3331192f57f316841b198317eccd04417ced1a0609614f6838114d1ad7b0495a386158502643cc4a4f3277a853012'
