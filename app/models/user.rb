52eccfa8725c206118e662d0916756658112314a
Carlos <carlosvilhena@gmail.com>
2011-04-07 11:41:28 +0100

    added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessor :password_confirmation
-  validates_presence_of :name, :email, :username, :password
+  validates_presence_of :name, :username, :password
   validates_uniqueness_of :username
+
+  validates :email, :presence => true, :email => true
 #  validates :country, :allow_nil => true
 #  validates :password, :password => true
 end
