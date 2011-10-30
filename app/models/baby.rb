287447720eec01e0445e4d3188a437b9870ddebb
carvil <carlosvilhena@gmail.com>
2011-10-30 15:09:28 +0000

    deleted the baby model (will be re-generated) and the validators (devise will be used)

--- app/models/baby.rb
+++
@@ -1,9 +0,0 @@
-class Baby < ActiveRecord::Base
-
-  validates_presence_of :user_id, :name, :birthday
-
-  validates_inclusion_of :gender, :in => %w( male female ), :message => "invalid gender"
-
-  belongs_to :user
-
-end
