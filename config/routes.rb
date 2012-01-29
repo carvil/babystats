4a5ba7781d34af7c38d037c93b9068aa4ee8bc43
carvil <carlosvilhena@gmail.com>
2012-01-29 22:02:36 +0000

    added home controller and fixed routes + layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Babystats::Application.routes.draw do
-  get "welcome/index"
-
-  devise_for :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +49,9 @@ Babystats::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  devise_for :users do
+    root :to => "home#index"
+  end
 
   # See how all your routes lay out with "rake routes"
 
