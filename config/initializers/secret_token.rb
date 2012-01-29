fbf922b628faee3b9f7b35fba66aae75111a9f02
carvil <carlosvilhena@gmail.com>
2012-01-29 18:21:45 +0000

    basic rails 3.2 project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babystats::Application.config.secret_token = 'bdab104189507380a5bedc8cdfa8c25b9a841a717ad1aa88aaf827691f8f3d56daa62956569cd9dbf5f47175b50e0d1010d4fb31b7eedb68d69642add767ba3d'
