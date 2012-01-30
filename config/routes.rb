2b66debd3c26b8892939311108eb1840a2684124
carvil <carlosvilhena@gmail.com>
2012-01-30 23:08:49 +0000

    fixed routes - devise_for describes which model has devise; devise_scope scopes the index page to users (in order to show sign_up page)

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,9 @@ Babystats::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  devise_for :users do
+  devise_for :users
+
+  devise_scope :users do
     root :to => "home#index"
   end
 
