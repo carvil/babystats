e221ab8ea84b4d155297d33e0ab8696c90dfb973
carvil <carlosvilhena@gmail.com>
2012-01-30 22:34:25 +0000

    show multiple error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def build_message_div(key, messages = [])
+    unless messages.empty?
+      content_tag(:div, :class => "alert-message #{key}", "data-alert" => "alert") do
+        concat link_to "x", "#", :class => "close"
+        messages.each do |error_message|
+          concat content_tag :p, "- #{error_message}"
+        end
+      end
+    end
+  end
 end
