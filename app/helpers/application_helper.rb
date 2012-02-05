020dd8641141d8696c00404c94e941f746aebbaf
carvil <carlosvilhena@gmail.com>
2012-02-05 21:40:46 +0000

    refactored messages' div to display a list of messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,17 @@
 module ApplicationHelper
   def build_message_div(key, messages = [])
     unless messages.empty?
-      content_tag(:div, :class => "alert-message #{key}", "data-alert" => "alert") do
+      content_tag(:div, :class => "alert-message block-message #{key}", "data-alert" => "alert") do
         concat link_to "x", "#", :class => "close"
-        messages.each do |error_message|
-          concat content_tag :p, "- #{error_message}"
-        end
+        concat content_tag :p, "Oh snap! You got errors. Please change and try again."
+        concat build_messages_list messages
       end
+    end
+  end
+
+  def build_messages_list messages
+    content_tag :ul do
+      messages.collect {|message| concat(content_tag(:li, message.to_s)) }
     end
   end
 end
