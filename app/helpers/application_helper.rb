2059e4e59293e8eb379529900c9bdacd44a90f9e
Carlos Vilhena <carlosvilhena@gmail.com>
2012-02-11 18:18:48 +0000

    converting alerts to bootstrap2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def build_message_div(key, messages = [])
     unless messages.empty?
-      content_tag(:div, :class => "alert-message block-message #{key}", "data-alert" => "alert") do
+      content_tag(:div, :class => "alert alert-#{key}") do
         concat link_to "x", "#", :class => "close"
         concat content_tag :p, "Oh snap! You got errors. Please change and try again."
         concat build_messages_list messages
